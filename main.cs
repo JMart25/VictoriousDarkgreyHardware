426b716e0109106646d1765b90b07e9c703f4f85
JMart25 <149201983+JMart25@users.noreply.github.com>
2024-05-20 15:36:10 +0000

    Updates

--- main.cs
+++ main.cs
@@ -4,10 +4,10 @@ class PersonalInfo
 {
     static void Main()
     {
-      string name = "John Smith";
-      string birthdate = "8/15/1978";
-      string workPhoneNumber = "000-111-0101";
-      string cellPhoneNumber = "000-111-0189";
+      string name = "Robert Hall";
+      string birthdate = "2/27/1949";
+      string workPhoneNumber = "000-000-0101";
+      string cellPhoneNumber = "000-000-0189";
 
         Console.WriteLine(name);
         Console.WriteLine(birthdate);
@@ -21,6 +21,13 @@ class PersonalInfo
 
 
 /*
+string name = "Lucinda Potter";
+string birthdate = "6/24/1992";
+string workPhoneNumber = "000-000-0101";
+string cellPhoneNumber = "000-000-0189";
+
+------------------------------------------
+
 string name = "John Smith";
 string birthdate = "8/15/1978";
 string workPhoneNumber = "000-111-0101";
