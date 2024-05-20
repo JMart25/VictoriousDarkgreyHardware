183fc8bdbe288acfabfd92b2a56fecdd64711292
JMart25 <149201983+JMart25@users.noreply.github.com>
2024-05-20 15:28:25 +0000

    Updates

--- main.cs
+++ main.cs
@@ -1,7 +1,43 @@
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World, it's John Marino!");
-  }
+class PersonalInfo
+{
+    static void Main()
+    {
+      string name = "John Smith";
+      string birthdate = "8/15/1978";
+      string workPhoneNumber = "000-111-0101";
+      string cellPhoneNumber = "000-111-0189";
+
+        Console.WriteLine(name);
+        Console.WriteLine(birthdate);
+        Console.WriteLine("work " + workPhoneNumber);
+        Console.WriteLine("cell " + cellPhoneNumber);
+    }
 }
+
+// OTHER TEST CASES 
+//JUST COPY AND PASTE BELOW TEST CASES BETWEEN static void MAIN() and the console.WriteLines 
+
+
+/*
+string name = "John Smith";
+string birthdate = "8/15/1978";
+string workPhoneNumber = "000-111-0101";
+string cellPhoneNumber = "000-111-0189";
+
+------------------------------------------
+
+string name = "Chris Johnson";
+string birthdate = "12/01/1987";
+string workPhoneNumber = "000-000-0101";
+string cellPhoneNumber = "000-000-0189";
+
+------------------------------------------
+
+string name = "Robert Hall";
+string birthdate = "2/27/1949";
+string workPhoneNumber = "000-000-0101";
+string cellPhoneNumber = "000-000-0189";
+
+*/
